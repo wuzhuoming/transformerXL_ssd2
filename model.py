efa19d102eb1df585392a4361c3c10567260d0e3
wuzhuoming <31766329+wuzhuoming@users.noreply.github.com>
2020-12-29 02:13:23 +0800

    Update model.py

--- model.py
+++ model.py
@@ -413,12 +413,12 @@ def mul_adaptive_logsoftmax(hidden, target, n_token, d_embed, d_proj, cutoffs,
 
 def _create_mask(qlen, mlen, same_length=False):
   attn_mask = tf.ones([qlen, qlen])
-  mask_u = tf.matrix_band_part(attn_mask, 0, -1)
-  mask_dia = tf.matrix_band_part(attn_mask, 0, 0)
+  mask_u = tf.compat.v1.matrix_band_part(attn_mask, 0, -1)
+  mask_dia = tf.compat.v1.matrix_band_part(attn_mask, 0, 0)
   attn_mask_pad = tf.zeros([qlen, mlen])
   ret = tf.concat([attn_mask_pad, mask_u - mask_dia], 1)
   if same_length:
-    mask_l = tf.matrix_band_part(attn_mask, -1, 0)
+    mask_l = tf.compat.v1.matrix_band_part(attn_mask, -1, 0)
     ret = tf.concat([ret[:, :qlen] + mask_l - mask_dia, ret[:, qlen:]], 1)
   return ret
 
